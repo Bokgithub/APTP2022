c4056929f43c0dda13d94ac8cefe734f966c854d
8taby <97948617+8taby@users.noreply.github.com>
2022-05-04 16:03:20 +0900

    python

--- test.py
+++ test.py
@@ -1,15 +1,6 @@
-import json
+import songdo
+import sinchon
 
-with open("Asterisk\songdo.json", "r", encoding="utf8") as f:
-  data = f.read()
-  jsdt = json.loads(data)
-
-source = input("출발지: ")
-# 출발지를 입력받은 뒤
-for i in range(len(jsdt["Building"])):
-  if jsdt["Building"][i]["name"] == source:
-    source_id = jsdt["Building"][i]["id"]
-    print(source_id)
-    for j in range(len(jsdt["Building"][i]["neighbor"])):
-      neighbor_node = jsdt["Building"][i]["neighbor"][j]
-      print(neighbor_node)
+print(songdo.NodeSongdo)
+type = type(songdo.NodeSongdo)
+print(type)
